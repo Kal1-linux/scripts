657a629438e06906d63425ea7d3005f5d056527d
root <root@example.com>
2023-06-02 01:16:04 -0700

    docker script done

---
+++ docker.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt install apt-transport
+curl -fsSL ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64]
+sudo apt-get update
+apt install docker.io -y
