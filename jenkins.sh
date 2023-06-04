c7e231b9d1cb3f5dc57e3c18664283cf30f4a20a
Kal1-linux <84131564+Kal1-linux@users.noreply.github.com>
2023-06-04 21:35:19 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,5 +4,10 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
 sudo apt install openjdk-11-jre -y
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
