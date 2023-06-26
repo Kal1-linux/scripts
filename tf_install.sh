4af6024e2393ceef106fa292a62d667877501c36
Kal1-linux <84131564+Kal1-linux@users.noreply.github.com>
2023-06-26 18:28:57 +0530

    Update tf_install.sh
    done

--- tf_install.sh
+++ tf_install.sh
@@ -1,4 +1,11 @@
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
