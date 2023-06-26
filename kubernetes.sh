5d2cf6b9680e31cd91a8232e6eebbbd5a9313f40
Kal1-linux <84131564+Kal1-linux@users.noreply.github.com>
2023-06-26 18:26:34 +0530

    Update kubernetes.sh
    done

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,15 +1,16 @@
-apt-get update -y
-apt-get install curl wget apt-transport-https -y
-apt install docker.io -y
-wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-cp minikube-linux-amd64 /usr/local/bin/minikube
-chmod 755 /usr/local/bin/minikube
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update -y
-apt-get install kubectl -y
+sudo apt-get update -y
+sudo apt-get install curl wget apt-transport-https -y
+sudo apt install docker.io -y
+sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod 755 /usr/local/bin/minikube
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install kubectl -y
 kubectl version -o json
 sudo usermod -aG docker $USER && newgrp docker
+sudo chmod 777 /var/lib/docker.sock
 minikube start --driver=docker
 
 
